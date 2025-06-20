63c975d6d23740fc6121be46757dad1496a2b4df
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-06-20 13:39:50 +0530

    separated jest config
    Signed-off-by: Karteek <120569182+karteekiitg@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
